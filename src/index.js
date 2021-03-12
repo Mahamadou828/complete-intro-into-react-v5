3b686c25ec6baddfbc4d395b346447bee4649fe3
Mahamadou Samaké <59400249+Mahamadou828@users.noreply.github.com>
2021-03-12 15:18:10 +0300

    adding lazy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { Link, Router } from '@reach/router';
-import React, { useState } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { render } from 'react-dom';
 import { SearchParam } from './components/SearchParam';
 import ThemeContext from './context/theme';
-import Details from './pages/Details';
+const Details = lazy(() => import('./pages/Details'));
 
 const App = () => {
   const themeHook = useState('darkblue');
@@ -13,11 +13,12 @@ const App = () => {
         <header>
           <Link to="/">Adopt Me</Link>
         </header>
-
-        <Router>
-          <SearchParam path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        <Suspense fallback={<h1>Loading Route</h1>}>
+          <Router>
+            <SearchParam path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
